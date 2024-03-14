9622297300107e68e83360158992f5637daf0c11
MeyZo0 <vgadstrev@gmail.com>
2024-03-14 14:14:06 +0200

    створив заготовку

--- app/src/main/java/ua/edu/lntu/cw_cr4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_cr4/MainActivity.kt
@@ -3,13 +3,27 @@ package ua.edu.lntu.cw_cr4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_cr4.ui.theme.IPZ_CW_CR4_Syshchuk_VladislavTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainWindow()
                 }
             }
         }
@@ -30,17 +44,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_CR4_Syshchuk_VladislavTheme {
-        Greeting("Android")
+fun MainWindow() {
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,) {
+        Text(
+            text = "Список завдань",
+            fontSize = 30.sp,
+        )
+        LazyColumn(
+            contentPadding = PaddingValues(10.dp),
+            content = {
+                item {
+                    item1()
+                    Spacer(modifier = Modifier.height(20.dp))
+                    item2()
+                    Spacer(modifier = Modifier.height(20.dp))
+                    item3()
+                }
+            }
+        )
     }
 }
+
+@Composable
+fun item1 () {
+    Column (modifier = Modifier
+        .background(Color.LightGray)
+        .clip(shape = RoundedCornerShape(6.dp))
+        .padding(15.dp)
+    )
+    {
+
+        Text(
+            text = "10:00",
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
+        )
+    }
+
+}
+
+@Composable
+fun item2 () {
+    Column (modifier = Modifier
+        .background(Color.LightGray)
+        .clip(shape = RoundedCornerShape(6.dp))
+        .padding(15.dp)
+    ) {
+
+        Text(
+            text = "12:00",
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )
+    }
+
+}
+
+@Composable
+fun item3 () {
+    Column (modifier = Modifier
+        .background(Color.LightGray)
+        .clip(shape = RoundedCornerShape(6.dp))
+        .padding(15.dp)
+    ) {
+
+        Text(
+            text = "18:00",
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )
+    }
+
+}
+
